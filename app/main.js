3e74c2860d4642daf35bbc8b7e41163c7127d683
Datior <leonardowederveiga@yahoo.com>
2023-03-08 18:09:20 -0300

    Map: cria um novo array com as modificações

--- app/main.js
+++ app/main.js
@@ -1,6 +1,5 @@
 var livros = []
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
-const elementoParaInserirLivros = document.getElementById('livros')
 
 getBuscarLivros()
 
@@ -8,23 +7,12 @@ async function getBuscarLivros() {
     const resposta = await fetch(endpointDaAPI)
     livros = await resposta.json()
 
+    let livrosComDesconto = aplicarDisconto(livros)
     console.table(livros)
 
-    exibirLivros()
+    exibirLivros(livrosComDesconto)
 }
 
-function exibirLivros() {
-    livros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `<div class="livro">
-          <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-          <h2 class="livro__titulo">
-            ${livro.titulo}
-          </h2>
-          <p class="livro__descricao">${livro.autor}</p>
-          <p class="livro__preco" id="preco">${livro.preco}</p>
-          <div class="tags">
-            <span class="tag">${livro.categoria}</span>
-          </div>
-        </div>`
-    })
-}
+
+
+
